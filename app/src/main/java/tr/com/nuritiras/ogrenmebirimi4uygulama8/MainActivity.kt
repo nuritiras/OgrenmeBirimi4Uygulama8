3fd97e05beef2ec27e07331e05e215e207b799df
Nuri TIRAŞ <nuritiras@gmail.com>
2024-11-15 14:26:57 +0300

    for loop step 3

---
+++ app/src/main/java/tr/com/nuritiras/ogrenmebirimi4uygulama8/MainActivity.kt
@@ -0,0 +1,30 @@
+package tr.com.nuritiras.ogrenmebirimi4uygulama8
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+
+        println("-------Döngü başladı-------")
+        for (i in 0..100 step 3) {
+            println(i)
+        }
+        println("-------Döngü bitti-------")
+
+    }
+}
